bec51850516be553adc368ffe0de5a997355706d
Oloo Ouma <oloosolomon@gmail.com>
2019-06-17 06:56:46 +0300

    Write code for adding a user

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ class User :
     def __init__(self, username, account,password):
         self.username = username
         self.account = account
-        self.password = password
+        self.password = passwor
 
     def save_user(self):
         '''
